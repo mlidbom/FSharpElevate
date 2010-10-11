d2cbdf4462411994d82adbe742915e5e08ecc7bd
malm <magnus.lidbom@gmail.com>
2010-10-12 01:47:44 +0200

    Polishing.

--- FSharp/InterfacesClasses.fsx
+++ FSharp/InterfacesClasses.fsx
@@ -6,14 +6,21 @@ type IPrintable =
 [<AbstractClassAttribute>]//Required for abstract classes
 type AbstractPrintable(text) = //Constructor
     interface IPrintable with
-        member me.Print() = printf "%s" text |> ignore
+        member me.Print() = printfn "%s" text |> ignore
 
     //Abstract method..
     abstract member DoSomething : unit -> unit
-   
-    //Read only property
-    member me.Text with get() = text;
 
+    //Read only property
+    member me.Text = text;
+    
+    //Identical to more verbose:
+    //member me.Text with get() = text;
+
+    abstract member OverrideMe : unit -> unit
+    //This is how you create "virtual" members in f#
+    //provide a default implementation of the member
+    default me.OverrideMe() = ()
 
 [<SealedAttribute>]//No overriding
 type PrintableVector(x:int, y: int) =
@@ -25,10 +32,24 @@ type PrintableVector(x:int, y: int) =
     override this.DoSomething() = printfn "Doing Something" |> ignore
 
     //Notice how the Self Identifiers need not be named "Me" or "this" etc
-    member you.X with get() = x
-    member me.Y with get() = x
+    member you.X = x
+    member me.Y = x
+
+    override me.OverrideMe() = printfn "%s" "Overridden" |> ignore
 
 
 
+let zenith = new PrintableVector(0, 1)
 
+let one = zenith.Y
+let zero = zenith.X
+
+zenith.DoSomething()
+
+
+//zenith.Print() does not compile!
+//You have to cast to interface types.
+(zenith :> IPrintable).Print();
+
+//It's a safe cast though
 
--- FSharp/Mutability.fsx
+++ FSharp/Mutability.fsx
@@ -5,15 +5,29 @@ x <- 7
 let seven = x
 
 //Classes
-
 type MutableArray(x:int, y:int) =
     let mutable _x = x
     let mutable _y = y
 
     //Get set properties
-    member this.X with get() = _x and set(newX) = _x <- newX
+    member this.X with get() = _x and set newX = _x <- newX
     
     //Pick your formatting poison
     member this.Y 
         with get() = _y 
-        and set(newX) = _y <- newX
+        and set newX = _y <- newX
+
+let variable = new MutableArray(1,2)
+
+let one = variable.X
+let two = variable.Y
+
+variable.X <- 3
+let three =variable.X
+
+//Records
+type Vec2 = { mutable X : int; mutable Y: int}
+
+let temp = { X=1; Y=1}
+temp.X <- 2
+temp.Y <-2
