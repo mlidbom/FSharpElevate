37274cc9c56da06ebda43e5a0fae0470429bd113
malm <magnus.lidbom@gmail.com>
2010-10-12 01:18:06 +0200

    Minor update

--- FSharp/PuttingOperatorsToUse.fsx
+++ FSharp/PuttingOperatorsToUse.fsx
@@ -1,6 +1,5 @@
-﻿let divisibleBy x y = x % y = 0
-
-let isPrime x = 
+﻿let isPrime x = 
+    let divisibleBy x y = x % y = 0
     x > 2 && //By definition 2 is not prime
     seq{2..(x-1)} //All numbers less than x
     |> Seq.forall (not << (divisibleBy x) ) //Must not evenly divide x 
