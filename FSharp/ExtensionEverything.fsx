5d73376fa820087f862c8526f155d5e7bb22c2d9
malm <magnus.lidbom@gmail.com>
2010-10-11 23:42:30 +0200

    ...

---
+++ FSharp/ExtensionEverything.fsx
@@ -0,0 +1,20 @@
+﻿module CollectionExtensions =
+  //Extension method
+  type System.String with
+    member this.IsNullOrEmpty() = System.String.IsNullOrEmpty this
+    
+    //Static extension method
+    static member MyLength (aString:string) = aString.Length
+
+    //Extension property  
+    member this.NumberOfAs with get() = this |> Seq.filter (fun c -> c = 'a') |> Seq.length
+
+
+
+open CollectionExtensions
+open System
+
+let two = "aoeua".NumberOfAs
+let truth = "".IsNullOrEmpty()
+
+let five = String.MyLength "abcde"
